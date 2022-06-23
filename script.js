57dc6eb3b581d6c21fc26a760ac0636833b34672
RyanLarge13 <101679941+RyanLarge13@users.noreply.github.com>
2022-06-22 17:52:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
- 
+
  // card display functiom that animates the more button at the bottom of the page and displays the cards section
- const firstArticle = document.querySelector('.article-more');
- const moreButton = document.querySelector('#more-btn');
- 
- 
+const firstArticle = document.querySelector('.article-more');
+const moreButton = document.querySelector('#more-btn');
 const moreButtonTag = document.querySelector('.more-sec-4-tag');
+const wrapper = document.querySelector('.cards-wrapper');
 
 
 const cardDisplay = () => {
@@ -17,6 +16,8 @@ const cardDisplay = () => {
     moreButtonTag.textContent = 'CLOSE';
   }
   else if (firstArticle.style.height == '200vh') {
+    wrapper.style.opacity = '0';
+    wrapper.style.transition = 'opacity 250ms linear';
     firstArticle.style.transform = "scale(1, 0)";
     firstArticle.style.height = '0';
     moreButton.classList.remove('rev');
@@ -25,12 +26,20 @@ const cardDisplay = () => {
     moreButtonTag.textContent = 'MORE';
   }
   else {
+    setTimeout(() => {
+     wrapper.style.opacity = '1';
+     wrapper.style.transition = 'opacity 2s'
+    }, 1000)
     firstArticle.style.transform = "scale(1, 1)";
     firstArticle.style.height = '200vh';
     moreButton.classList.add('rev');
     moreButton.classList.remove('more-btn');
     moreButton.style.animationName = ('more-btn-rev');
     moreButtonTag.textContent = 'CLOSE';
+    
+    setTimeout(function() {
+      firstArticle.scrollIntoView({block: 'start', inline: 'start', behavior: "smooth"});
+    }, 600);
   }
 }
 
@@ -111,7 +120,6 @@ window.onload = secOneAppear = () => {
   //declairing sec 1 variables
   const secOneHeading = document.querySelector('.sec-1 h1');
   const secOneParagraph = document.querySelector('.sec-1 p');
-  const secOneButton = document.querySelector('.intro-btn');
   
   //styles for variables
     secOneHeading.style.opacity = '1';
@@ -126,37 +134,222 @@ window.onload = secOneAppear = () => {
 const links = document.querySelectorAll("nav ul a");
 const moreBtnLink = document.querySelector('.more-btn-link');
 
-for (const link of links) {
-  link.addEventListener("click", clickHandler);
-}
-
-function clickHandler(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  const offsetTop = document.querySelector(href).offsetTop;
-
-  scroll({
-    top: offsetTop,
-    behavior: "smooth"
-  });
-}
-
-
-
-
-
-
-
-
-
-
-
 // card dynamics
 const cards = document.querySelectorAll('#card');
-const eachCardHeading = document.querySelectorAll('#card h3');
+let arrCards = Array.from(cards);
+
+const cardBg = (e) => {
+  
+  for (let k = 0; k < cards.length; k++) {
+    let cardClicked = e.target;
+    
+    if (cardClicked == cards[k]) {
+      cards[k].classList.add('card-flip');
+      cards[k].nextElementSibling.style.opacity = '1';
+    } else if (cardClicked.classList.contains('card-flip')) {
+      cards[k].classList.remove('card-flip');
+      cards[k].nextElementSibling.style.opacity = '0';
+    }
+  }
+};
 
 cards.forEach(card => {
-  card.addEventListener('click', () => {
+  card.addEventListener('click', (e) => {
     card.classList.toggle('card-flip');
+    cardBg(e);
   });
 });
+
+const cardBgBtn = document.querySelectorAll('.card-bg-btn');
+let arrOfBgBtn = Array.from(cardBgBtn);
+
+ const cardBtn = (e) => {
+   
+  for (let k = 0; k < cardBgBtn.length; k++) {
+    cards[arrOfBgBtn.indexOf(e.target)].classList.remove('card-flip');
+    cards[k].nextElementSibling.style.opacity = '0';
+  }
+};
+
+cardBgBtn.forEach(btn => {
+  btn.addEventListener('click', cardBtn)
+  });
+  
+  //sample slider
+  const sampleContainer = document.querySelector('.sample-product-container');
+  const sampleSlider = document.querySelector('.sample-product-holder');
+  const sampleProduct = document.querySelectorAll('.sample-product');
+  const sampleDots = document.querySelectorAll('.sample-dots div');
+  
+  const move = (e) => {
+    e.preventDefault();
+    let left = e.pageX;
+    let leftO = -2 * (206 - e.pageX) + 206;
+    
+    if (left > 100 && left < 300) {
+      sampleDots[0].style.opacity = '.4';
+      sampleDots[1].style.opacity = '1';
+      sampleDots[2].style.opacity = '.4';
+    }
+    if (left > 300) {
+      sampleDots[0].style.opacity = '1';
+      sampleDots[1].style.opacity = '.4';
+      sampleDots[2].style.opacity = '.4';
+    }
+    if (left < 100) {
+      sampleDots[0].style.opacity = '.4';
+      sampleDots[1].style.opacity = '.4';
+      sampleDots[2].style.opacity = '1';
+    }
+    
+    sampleSlider.style.left = left + 'px';
+    sampleSlider.style.transition = 'none';
+      
+    if (left > 206) {
+      sampleSlider.style.left = left * 2 + 'px';
+      sampleProduct.forEach(item => {
+        item.style.transform = 'translateX(-78%)'
+    });
+    }
+      
+    if (left < 206) {
+      sampleSlider.style.left = leftO + 'px';
+      sampleProduct.forEach(item => {
+        item.style.transform = 'translateX(0)'
+    });
+    }
+  }
+  
+  sampleContainer.addEventListener('pointermove', move);
+  
+  
+  
+  
+  
+  //switching between bodies
+  //body 1
+  const featuresBtn = document.querySelector('.intro-btn');
+  const productBtn = document.querySelector('.products-btn');
+  const allBodies = document.querySelectorAll('#bodies');
+  const bodyOne = document.querySelector('.body-1');
+  const bodyTwo = document.querySelector('.body-2');
+  const bodyThree = document.querySelector('.body-3')
+  
+  allBodies.forEach(body => {
+    body.style.transition = '750ms';
+  });
+  
+
+  productBtn.addEventListener('click', () => {
+    bodyThree.style.height = '100vh';
+    bodyThree.style.right = '0';
+    bodyOne.style.transform = 'translateX(100vw)';
+    setTimeout(() => {
+      bodyThree.style.top = '0vh';
+      bodyOne.style.height = '100vh';
+    }, 750);
+    
+    navListThree()
+  });
+  
+  featuresBtn.addEventListener('click', () => {
+    bodyTwo.style.height = 'max-content';
+    bodyTwo.style.right = '0';
+    bodyOne.style.transform = 'translateX(-100vw)';
+    setTimeout(() => {
+      bodyOne.style.height = '100vh';
+    }, 750);
+    
+    navListTwo();
+  })
+  
+  //body 2
+  const goBackToMainFrom2 = document.querySelector('.back-to-main-from-2');
+  const goBackToMainFrom3 =
+  document.querySelector('.back-to-main-from-3');
+  
+  goBackToMainFrom2.addEventListener('click', () => {
+    bodyTwo.style.right = '-100vw';
+    bodyOne.style.transform = 'translateX(0)';
+    bodyOne.style.height = 'unset';
+    
+    setTimeout(() => {
+      bodyTwo.style.height = '100vh';
+    }, 750);
+    
+    navListMain();
+  });
+  
+  goBackToMainFrom3.addEventListener('click', () => {
+    bodyThree.style.right = '100vw';
+    bodyOne.style.transform = 'translateX(0)';
+    bodyOne.style.height = 'unset';
+    
+    setTimeout(() => {
+      bodyThree.style.height = '100vh';
+      bodyThree.style.top = '100vh';
+    }, 750);
+    
+    navListMain();
+  });
+  
+  
+  
+  //switching between nav list items
+  const allNavItems = document.querySelectorAll('nav ul li');
+  
+  const navListMain = () => {
+    allNavItems[0].firstChild.href = '#products';
+    allNavItems[1].firstChild.href = '#about';
+    allNavItems[2].firstChild.href = '#contact';
+    allNavItems.forEach(link => {
+      link.style.opacity = '0';
+    });
+    setTimeout(() => {
+      allNavItems.forEach(link => {
+        link.style.opacity = '1';
+      });
+      allNavItems[0].firstChild.innerHTML = 'Products';
+      allNavItems[1].firstChild.innerHTML = 'About';
+      allNavItems[1].firstChild.style.padding = '1em 1.3em';
+      allNavItems[2].firstChild.innerHTML = 'Contact';
+    }, 375);
+  }
+  
+  
+  const navListTwo = () => {
+    allNavItems[0].firstChild.href = '#Subscribe';
+    allNavItems[1].firstChild.href = '#YouTube';
+    allNavItems[2].firstChild.href = '#Samples';
+    
+    allNavItems.forEach(link => {
+      link.style.opacity = '0';
+    });
+    setTimeout(() => {
+      allNavItems.forEach(link => {
+        link.style.opacity = '1';
+      });
+      allNavItems[0].firstChild.innerHTML = 'Subscribe';
+      allNavItems[1].firstChild.innerHTML = 'YouTube';
+      allNavItems[1].firstChild.style.padding = '1em .5em'
+      allNavItems[2].firstChild.innerHTML = 'Samples';
+    }, 375);
+  };
+  
+  const navListThree = () => {
+    allNavItems[0].firstChild.href = '#main';
+    allNavItems[1].firstChild.href = '#product-1';
+    allNavItems[2].firstChild.href = '#product-2';
+    
+    allNavItems.forEach(link => {
+      link.style.opacity = '0';
+    });
+    setTimeout(() => {
+      allNavItems.forEach(link => {
+        link.style.opacity = '1';
+      });
+      allNavItems[0].firstChild.innerHTML = 'Main';
+      allNavItems[1].firstChild.innerHTML = 'One';
+      allNavItems[2].firstChild.innerHTML = 'Two';
+    }, 375);
+  }
